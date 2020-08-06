fb538633aeda1579340ab00dd8d08ff72661eb89
Salv Barale <s_barale@yahoo.com>
2020-08-06 19:20:35 -0300

    Initial commit

---
+++ 01.syntax.py
@@ -0,0 +1,28 @@
+#Execute Python Syntax
+#As we learned in the previous page, Python syntax can be executed by writing directly in the Command Line:
+
+#Where in other programming languages the indentation in code is for readability only,
+#the indentation in Python is very important.
+
+
+#Example
+if 5 > 2:
+  print("Five is greater than two!")
+#Python will give you an error if you skip the indentation:
+
+
+#Example
+if 5 > 3:
+ print("Five is greater than three!")
+if 5 > 4:
+        print("Five is greater than four!")
+
+
+#Example
+#Variables in Python:
+
+x = 5
+y = "Hello, World!"
+
+#Comments start with a #, and Python will render the rest of the line as a comment:
+
